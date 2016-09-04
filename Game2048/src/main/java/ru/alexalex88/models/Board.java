8a5e1e8d8b784f0fd225d3e3e182c1cc62a0f6ba
alexalex88 <alex-alex-88@mail.ru>
2016-09-05 02:54:51 +0300

    Board: change comments to annotations Board: add swipeRow, swipeColumn functions

--- Game2048/src/main/java/ru/alexalex88/models/Board.java
+++ Game2048/src/main/java/ru/alexalex88/models/Board.java
@@ -4,46 +4,90 @@ package ru.alexalex88.models;
  * Created by Alex on 05.09.2016.
  */
 public class Board {
-    //represents game board
+    /**
+     * represents game board
+     */
     private int[][] board = new int[4][4];
 
-    //number of blank squares on the board
+    /**
+     * number of blank squares on the board
+     */
     private int blankSquares;
 
-    //getter for board
+    /**
+     * getter for board
+     * @return return array int[4][4] which contains values on the board
+     */
     public int[][] getBoard() {
         return board;
     }
 
-    //try to swipe squares right
+    /**
+     * try to swipe squares right
+     */
     public void swipeRight(){;}
 
-    //try to swipe squares left
+    /**
+     * try to swipe squares left
+     */
     public void swipeLeft(){;}
 
-    //try to swipe squares up
+    /**
+     * try to swipe squares up
+     */
     public void swipeUp(){;}
 
-    //try to swipe squares down
+    /**
+     * try to swipe squares down
+     */
     public void swipeDown(){;}
 
-    //start new game
+    /**
+     * start new game
+     */
     public  void newGame(){
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 board[i][j] = 0;
+        blankSquares = 16;
     }
 
-    //check if game is over
+    /**
+     * check if game is over
+     * @return true if game is over, false if game is on
+     */
     public boolean isGameOver(){
         return true;
     }
 
-    //generate new value on one of the blank squares
-    private void generateValue(){;}
+    /**
+     * generate new value on one of the blank squares
+     * new value is 2 or 4
+     * variable p stored probability of 2
+     */
+    private void generateValue(){
+        double p = 0.8;
+    }
 
-    //check if squares can be swiped
+    /**
+     * check if squares can be swiped
+     * @return true if squares can be swiped, false otherwise
+     */
     private boolean hasMove(){
         return false;
     }
+
+    /**
+     * swipe specified row
+     * @param i - number of swiped row
+     * @param right - swipe direction, true - right, false - left
+     */
+    private void swipeRow(int i, boolean right){;}
+
+    /**
+     * swipe specified column
+     * @param i - number of swiped column
+     * @param up - swipe direction, true - up, false - down
+     */
+    private void swipeColumn(int i, boolean up){;}
 }
